e026fa6c0785d07f02e9d57485af1769fcae1abf
Cathy D'Onofrio <catd825@gmail.com>
2020-05-26 02:04:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,19 +2,19 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   
-  outer_results=""
-  row_index=0
-    while row_index<src.length do
-      element_index=0
-      phrase=""
-        while element_index < src[row_index].length do
-          if src[row_index][element_index].is_a? String
-            phrase+=src[row_index][element_index] + " "
+  outer_results="" #start with blank string since we want to return a string
+  row_index=0 #start with first row
+    while row_index<src.length do #to ensure we do not loop beyond the lenght of the array
+      element_index=0 #define starting place for element
+      phrase="" #create blank phrase
+        while element_index < src[row_index].length do #while the element index is within the length of the row
+          if src[row_index][element_index].is_a? String #if the element chosen is a string
+            phrase+=src[row_index][element_index] + " " #return each element that is a string with a space
           end
-          element_index+=1
+          element_index+=1 #loop through every element in every row
         end
-        outer_results << phrase
-        row_index+=1
+        outer_results << phrase #push each string with space into new string
+        row_index+=1 #loop through every row
       end
-    outer_results
+    outer_results #return new value
 end
