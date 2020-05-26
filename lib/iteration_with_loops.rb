e718d6da41f0523417cc04673377afb0c9831671
Cathy D'Onofrio <catd825@gmail.com>
2020-05-26 01:47:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,6 +11,7 @@ def join_nested_strings(src)
           if src[row_index][element_index].is_a?(string)
             phrase=src[row_index][element_index]
           end
-          
+          element_index+=1
+        end
   
 end
