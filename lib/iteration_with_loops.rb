bb01e12f6bb7f1228fb4fae4a078f56505d2e906
Cathy D'Onofrio <catd825@gmail.com>
2020-05-26 01:51:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
       element_index=0
       phrase=""
         while element_index < src[row_index].length do
-          if src[row_index][element_index].is_a?(string)
+          if src[row_index][element_index].is_a? String
             phrase=src[row_index][element_index]
           end
           element_index+=1
